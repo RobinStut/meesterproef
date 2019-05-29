b32bff702da78952bc36d710bf0e40b1fd11c6ed
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-05-29 14:05:55 +0200

    Ignore this commit

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 
-
-
 // Node_modules
 const express = require("express")
 const bodyParser = require("body-parser")
 const session = require("express-session")
 const ejs = require("ejs")
-const apiKey = process.env.apiKey;
-const firebase = require("firebase");
-const admin = require("firebase-admin");
-const serviceAccount = require("./public/meesterproef-48b42-firebase-adminsdk-990t4-93c2b22a9f.json");
-
-require("dotenv").config()
-
-require("firebase/auth");
-require("firebase/firestore");
-require("dotenv-json")();
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://meesterproef-48b42.firebaseio.com"
-});
-
-const user = require("./modules/user.js");
 
 require("dotenv").config()
 
@@ -49,27 +30,6 @@ app.use(session({
   secret: "classified"
 }))
 
-var config = {
-  apiKey: apiKey,
-  authDomain: "meesterproef-48b42.firebaseapp.com",
-  databaseURL: "https://meesterproef-48b42.firebaseio.com",
-  storageBucket: "meesterproef-48b42.appspot.com"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-function writeUserData() {
-  firebase.database().ref("users/").set({
-    username: "test",
-  });
-}
-writeUserData()
-
-
-app.get("/", async (req, res) => {
-  res.render("pages/index")
-});
 // Routing
 require("./modules/routes.js")(app);
 
--- modules/user.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-module.exports = {
-  register: null,
-  login: null
-}
