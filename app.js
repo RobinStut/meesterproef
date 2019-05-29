01fea7913ccf5ed91c298d694e471a17e9474464
RobinStut <robin.stut@hva.nl>
2019-05-29 13:54:11 +0200

    rewriting firebase to module

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-
+require("dotenv-json")();
+require('dotenv').config()
 
 // Node_modules
 const express = require("express")
@@ -7,21 +7,6 @@ const bodyParser = require("body-parser")
 const session = require("express-session")
 const ejs = require("ejs")
 const apiKey = process.env.apiKey;
-const firebase = require("firebase");
-const admin = require("firebase-admin");
-const serviceAccount = require("./public/meesterproef-48b42-firebase-adminsdk-990t4-93c2b22a9f.json");
-
-require("dotenv").config()
-
-require("firebase/auth");
-require("firebase/firestore");
-require("dotenv-json")();
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://meesterproef-48b42.firebaseio.com"
-});
-
 const user = require("./modules/user.js");
 
 require("dotenv").config()
@@ -49,22 +34,8 @@ app.use(session({
   secret: "classified"
 }))
 
-var config = {
-  apiKey: apiKey,
-  authDomain: "meesterproef-48b42.firebaseapp.com",
-  databaseURL: "https://meesterproef-48b42.firebaseio.com",
-  storageBucket: "meesterproef-48b42.appspot.com"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-function writeUserData() {
-  firebase.database().ref("users/").set({
-    username: "test",
-  });
-}
-writeUserData()
+// Firebase
+require('./modules/firebase.js')();
 
 
 app.get("/", async (req, res) => {
