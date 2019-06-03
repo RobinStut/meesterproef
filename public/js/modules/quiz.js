f74fff28c4e1c4a4b71e8bfe5f78f1db1477f8ed
RobinStut <robin.stut@hva.nl>
2019-06-03 13:42:44 +0200

    completing basic functionality of quiz element

--- public/js/modules/quiz.js
+++ public/js/modules/quiz.js
@@ -1,10 +1,46 @@
 export default (() => {
-    console.log('test');
+    // console.log('test');
 
-    const sectionCount = document.getElementById("quizForm").getElementsByTagName('section')
+    const nextButton = document.getElementById('quizNextButton')
+    const backButton = document.getElementById('quizBackButton')
 
+    nextButton.addEventListener('click', e => {
+        e.preventDefault()
+        const sectionCount = document.getElementById("quizForm").getElementsByTagName('section')
+        const classNameFinder = document.getElementsByClassName('showFormElement')[0]
+        for (let i = 0; i < sectionCount.length; i++) {
+            const sectionClass = sectionCount[i].className;
 
+            if (sectionClass == 'showFormElement' && (i + 1) === sectionCount.length) {
+                nextButton.setAttribute('disabled', '')
 
+            } else {
+                classNameFinder.removeAttribute("class");;
+                classNameFinder.nextSibling.nextSibling.setAttribute('class', 'showFormElement');
+                console.log(backButton.removeAttribute('disabled', ''));
+
+            }
+        }
+    });
+
+    backButton.addEventListener('click', e => {
+        e.preventDefault()
+        const sectionCount = document.getElementById("quizForm").getElementsByTagName('section')
+        const classNameFinder = document.getElementsByClassName('showFormElement')[0]
+        for (let i = 0; i < sectionCount.length; i++) {
+            const sectionClass = sectionCount[i].className;
+            console.log(i);
+            if (sectionCount[0].className == 'showFormElement') {
+                backButton.setAttribute('disabled', '')
+            }
+            if (sectionClass == 'showFormElement' && i > 0) {
+                classNameFinder.removeAttribute("class");;
+                classNameFinder.previousSibling.previousSibling.setAttribute('class', 'showFormElement');
+                nextButton.removeAttribute('disabled', '')
+            }
+        }
+
+    });
 
 
 })()
