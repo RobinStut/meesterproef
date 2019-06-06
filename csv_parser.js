5ae8e3800a9f20e91e66e87be66b0f46396099ab
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-06 13:27:29 +0200

    Finished the parser

--- csv_parser.js
+++ csv_parser.js
@@ -1,5 +1,38 @@
 const fs = require("fs");
 
+class Object_template {
+  constructor(keys) {
+    this.keys = keys;
+    this.template = this.createTemplate()
+  }
+
+
+  createTemplate() {
+    let obj = {}
+
+    this.keys.forEach(k => {
+      obj[k] = undefined
+    })
+
+    return obj
+  }
+
+  createObject(row) {
+    const rawValues = row.split(",");
+    const keys = Object.keys(this.template)
+
+    rawValues.forEach((v, i) => {
+      if (v.length == 0) {
+        this.template[keys[i]] = undefined
+      } else {
+        this.template[keys[i]] = v
+      }
+    })
+
+    return this.template
+  }
+}
+
 fs.readFile("data/raw/sportaanbieders.csv", (err, data) => {
   if (err) throw err;
 
@@ -7,13 +40,28 @@ fs.readFile("data/raw/sportaanbieders.csv", (err, data) => {
 });
 
 function readCSV(data) {
-  const csv = data.toString();
+  const csv = data
+    .toString()
+    .replace(/\r/g, "");
+
   const rowsArray = csv.split(/\n/g)
   const namingsRow = rowsArray
     .shift()
     .toLowerCase()
-    .replace(/\r/g, "")
+
     .replace(/\s/g, "_")
     .split(",")
-console.log(namingsRow)
+
+  const template = new Object_template(namingsRow)
+
+  const finalData = rowsArray.map(r => {
+    return template.createObject(r)
+  })
+
+  const finalJSON = JSON.stringify(finalData)
+
+  fs.writeFile('data/json/sportaanbieders.json', finalJSON, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
 }
