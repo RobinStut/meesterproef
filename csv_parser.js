4d6205f99f58c4dcd157f596bc8514696d849f30
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-06 12:52:44 +0200

    Can split the csv into rows

---
+++ csv_parser.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+fs.readFile("data/raw/sportaanbieders.csv", (err, data) => {
+  if (err) throw err;
+
+  readCSV(data)
+});
+
+function readCSV(data) {
+  const csv = data.toString();
+  const rowsArray = csv.split(/\n/g)
+  const namingsRow = rowsArray.shift()
+}
