d2a2f7735bcb983141b0284b161b40d81bc2e82a
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-06 12:59:58 +0200

    The namings for the keys are now cleaner

--- csv_parser.js
+++ csv_parser.js
@@ -9,5 +9,11 @@ fs.readFile("data/raw/sportaanbieders.csv", (err, data) => {
 function readCSV(data) {
   const csv = data.toString();
   const rowsArray = csv.split(/\n/g)
-  const namingsRow = rowsArray.shift()
+  const namingsRow = rowsArray
+    .shift()
+    .toLowerCase()
+    .replace(/\r/g, "")
+    .replace(/\s/g, "_")
+    .split(",")
+console.log(namingsRow)
 }
