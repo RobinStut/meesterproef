a28bd9d7cb38b5882358396b54d75ef57684849d
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-11 11:01:52 +0200

    feature: Created header-sidebar.js module

---
+++ public/js/modules/header-sidebar.js
@@ -0,0 +1,14 @@
+function sidebarNav() {
+    const body = document.querySelector('body'),
+          headerHamburger = document.getElementById('st-hamburger-menu'),
+          headerHamburgerIcon = document.querySelector('#st-hamburger-menu svg'),
+          asideMenu = document.querySelector('aside')
+    
+    headerHamburger.addEventListener('click', () => {
+        body.classList.toggle('st-overflow-hidden')
+        asideMenu.classList.toggle('st-show')
+        headerHamburgerIcon.classList.toggle('st-text-color')
+    })
+}
+
+export { sidebarNav }
