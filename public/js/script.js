9b792d134cea9baeb34eba3cad8e5abea707d448
RobinStut <robin.stut@hva.nl>
2019-06-12 12:08:42 +0200

    fixing quiz slider bug

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,23 @@
     'use strict';
 
     import * as header from './modules/header.js'
-    import { sidebarNav } from './modules/header-sidebar.js'
-    import Quiz from "./modules/quiz-setup.js/index.js";
-    import CustomSlider from "./modules/custom-slider.js"
+    import {
+        sidebarNav
+    } from './modules/header-sidebar.js'
 
     const customSliders = document.getElementsByClassName("custom-slider")
 
     header.headerBackgroundScroll()
     sidebarNav()
+    import Quiz from "./modules/quizSetup.js";
+    import CustomSlider from "./modules/customSlider.js"
 
     (() => {
+        header.headerBackgroundScroll()
+        sidebarNav()
+
+        const customSliders = document.getElementsByClassName("custom-slider")
+
         for (let i = 0; i < customSliders.length; i++) {
             new CustomSlider(customSliders[i])
         }
