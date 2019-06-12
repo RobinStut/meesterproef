b4732d23a075efa06dc10dcb04b7a458e3a725d7
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-06-12 13:21:48 +0200

    feature: create dynamic heros

--- modules/routes.js
+++ modules/routes.js
@@ -1,6 +1,9 @@
 module.exports = app => {
   app.get("/", (req, res) => {
-    res.render("pages/index.ejs")
+    res.render("pages/index.ejs", {
+      hero: "big-hero", 
+      heroText: ["Amsterdam", "Zuid-Oost", "Be a part of it!"]
+    })
   })
   app.get("/quiz", (req, res) => {
     res.render("pages/quiz.ejs")
