d4c4c0314a3099532605fde4696d9cdc4dfd2b67
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-14 15:12:28 +0200

    feature(create-event): Finished create event functionality

--- modules/create-event.js
+++ modules/create-event.js
@@ -3,28 +3,38 @@ module.exports = (app) => {
 
     app.post('/created-event', (req, res) => {
         let event = {
-            title: req.body['event-name'],
-            description: req.body['event-description'],
-            location: req.body['event-location'],
-            date: req.body['event-date'],
-            timeStart: req.body['event-from-time'],
-            timeEnd: req.body['event-till-time']
+            general:{
+                sportProviderId: Math.random(),
+                title: req.body['event-name'],
+                description: req.body['event-description'],  
+            },
+            sport:{
+                sportName: req.body['event-sport'],
+                category: req.body['event-category']
+            },
+            location:{
+                city: req.body['event-city'],
+                address: req.body['event-address']
+            },
+            time:{
+                timeStart: req.body['event-from-time'],
+                timeEnd: req.body['event-till-time']
+            },
+            date: req.body['event-date']
         }
 
         if (allEvents.length === 0) {
             allEvents.push(event)   
         } else if (allEvents.length > 0) {
-            allEvents.map(e => {
-                if (e.title === event.title) {
-                    console.log('Event already exists')    
-                } else {
-                    console.log('Event added')
-                    allEvents.push(event)      
-                }      
+            const exists = allEvents.find(e => {
+                return e.general.title.toLowerCase() === event.general.title.toLowerCase()
             })
+            if (!exists) {
+                allEvents.push(event)
+            } else {
+                console.log("Sorry, that event already exists.")
+            }
         }
-
-        console.log(allEvents)
 
         res.render("pages/created-event.ejs", {
             hero: "small-hero",
