d1417d0cbb137177096fe3d9c52fb8df79e1afc6
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-14 11:48:05 +0200

    feature: working on datamodel

--- modules/create-event.js
+++ modules/create-event.js
@@ -3,19 +3,32 @@ module.exports = (app) => {
 
     app.post('/created-event', (req, res) => {
         let event = {
-            sportproviderInfo: {
-                name: 'test'
-            },
-            event: {
-                title: req.body['event-name'],
-                description: req.body['event-description'],
-                location: req.body['event-location'],
-                date: req.body['event-date'],
-                timeStart: req.body['event-from-time'],
-                timeEnd: req.body['event-till-time']
-            }
+            title: req.body['event-name'],
+            description: req.body['event-description'],
+            location: req.body['event-location'],
+            date: req.body['event-date'],
+            timeStart: req.body['event-from-time'],
+            timeEnd: req.body['event-till-time']
         }
 
-        console.log(event)
+        if (allEvents.length === 0) {
+            allEvents.push(event)   
+        } else if (allEvents.length > 0) {
+            allEvents.map(e => {
+                if (e.title === event.title) {
+                    console.log('Event already exists')    
+                } else {
+                    console.log('Event added')
+                    allEvents.push(event)      
+                }      
+            })
+        }
+
+        console.log(allEvents)
+
+        res.render("pages/created-event.ejs", {
+            hero: "small-hero",
+            heroText: ["Create Event"]
+        })
     })
 }
