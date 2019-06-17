9c005de64c4f0a70d0dd7b38209f25ba732c51a5
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-06-17 14:36:06 +0200

    feature: added detailted list for every sport

--- modules/routes.js
+++ modules/routes.js
@@ -13,10 +13,20 @@ module.exports = app => {
       hero: "small-hero", 
       heroText: ["Sports Activities A-Z"],
       data: data,
-      keys: Object.keys(data)
+      keys: Object.keys(data),
+    })
+  })
+  app.get("/sportslist/:id", async (req, res) => {
+    const id = req.params.id
+    const data = await sportslistData();
+    res.render("pages/sportlist-detail.ejs", {
+      hero: "small-hero", 
+      heroText: ["Sports Activities A-Z"],
+      sport: id,
+      data: data,
     })
   })
   app.get("/quiz", (req, res) => {
     res.render("pages/quiz.ejs")
   })
-}
+} 
