ee1ab793817062f6cfbe065d296f81451fc6a5fe
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-17 11:15:16 +0200

    feature(carousel): Added the carousel class that extends the draggingevent, with some boiler plate code

--- public/js/modules/carousel.js
+++ public/js/modules/carousel.js
@@ -1,1 +1,14 @@
 import DraggingEvent from "./dragging-event.js"
+
+const carouselContainer = document.querySelector(".card-carousel")
+
+class Carousel extends DraggingEvent {
+  constructor(container) {
+    super(container)
+
+    // Elements
+    this.container = container
+  }
+}
+
+new Carousel(carouselContainer)
