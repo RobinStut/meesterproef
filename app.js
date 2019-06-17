9dce9624933f3193940f63f111aee0ff4b10150c
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-17 14:50:35 +0200

    feature(app.js): Removed ; after require

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(session({ secret: "classified" }))
 
-require("./modules/routes.js")(app);
-require("./modules/create-event.js")(app, conceptEvents);
-require("./modules/publish-event.js")(app, conceptEvents, eventsData);
+require("./modules/routes.js")(app)
+require("./modules/create-event.js")(app, conceptEvents)
+require("./modules/publish-event.js")(app, conceptEvents, eventsData)
 
 app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
