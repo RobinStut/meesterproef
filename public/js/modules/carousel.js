086e270407a691819ee9183979a0a474efd663b9
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-17 11:32:24 +0200

    feature(carousel): Can now calculate the left positions for the cards (in percentages), based on the x value

--- public/js/modules/carousel.js
+++ public/js/modules/carousel.js
@@ -12,6 +12,7 @@ class Carousel extends DraggingEvent {
 
     // Carousel data
     this.centerIndex = (this.cards.length - 1) / 2; // center card
+    this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
 
     // Initalizer
     this.build()
@@ -22,14 +23,25 @@ class Carousel extends DraggingEvent {
       const x = i - this.centerIndex; // x-scale (-1 0 1)
       const scale = this.calcScale(x)
 
-      console.log(scale)
+      const leftPos = this.calcPos(x, scale)
+
+      console.log(leftPos)
+      this.cards[i].style.left = `${leftPos}%`
     }
   }
 
   calcScale(x) {
-    const formula = 1 - 1 / 5 * Math.pow(x, 2)
+    return 1 - 1 / 5 * Math.pow(x, 2)
+  }
 
-    return formula
+  calcPos(x, scale) {
+    if (x < 0) {
+      // Calculate the left position for the cards one the left side
+      return (scale * 100 - this.cardWidth) / 2;
+    } else {
+      // Calculate the left position for the cards on the right side
+      return 100 - (scale * 100 + this.cardWidth) / 2
+    }
   }
 }
 
