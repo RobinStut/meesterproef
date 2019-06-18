a781813039c6c10cce4909451816bade33aea9a3
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-18 11:28:59 +0200

    feature(write-to-json)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const session = require("express-session")
 const ejs = require("ejs")
+const fs = require('fs')
 const app = express()
 const PORT = 3000
 
@@ -22,7 +23,7 @@ app
 require("./modules/routes.js")(app, eventsData)
 require("./modules/quizPostRequest.js")(app);
 require("./modules/create-event.js")(app, conceptEvents)
-require("./modules/publish-event.js")(app, conceptEvents, eventsData)
+require("./modules/publish-event.js")(app, fs, conceptEvents, eventsData)
 require("./modules/quiz.js")(app);
 
 app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
