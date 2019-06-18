8c99c4019ade1b8b6d9548b6995d6ebe6ffe6bd3
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-18 10:00:13 +0200

    feature(events-page): Updated fetch url

--- modules/fetch.js
+++ modules/fetch.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 module.exports = () => {
     return new Promise(async (resolve, reject) => {
 
-        const response = await fetch('https://raw.githubusercontent.com/RobinStut/meesterproef/serverQuiz/public/sportQuizFilter.json'),
+        const response = await fetch('https://raw.githubusercontent.com/RobinStut/meesterproef/development/data/json/sportQuizFilter.json'),
               data = await response.json()
 
         resolve(data)
