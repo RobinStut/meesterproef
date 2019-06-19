b4351d0f54b6426360d486cff5f688b29a25f6d5
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-19 11:02:07 +0200

    refactor(client-js): Added the method that creates the settings for a custom slider

--- public/js/modules/custom-slider.js
+++ public/js/modules/custom-slider.js
@@ -10,4 +10,21 @@ export default class extends DraggingEvent {
       this.slider = this.createSlider()
       this.scale = this.createScale()
     }
+
+    createSettings() {
+      const settings = {
+        min: Number(this.rangeInput.min),
+        max: Number(this.rangeInput.max),
+        stepSize: Number(this.rangeInput.step) || 1,
+        value: Number(this.rangeInput.value)
+      }
+
+      settings.total = settings.max - settings.min;
+      settings.steps = settings.total / settings.stepSize;
+
+      return settings
+    }
+
+    createSlider() {}
+    createScale() {}
 }
