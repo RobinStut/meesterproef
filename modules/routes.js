d09b4a9a202f0c22a134d0e9c5ebaf40485520a7
RobinStut <robin.stut@hva.nl>
2019-06-19 14:33:57 +0200

    refactor/changing

--- modules/routes.js
+++ modules/routes.js
@@ -1,11 +1,12 @@
+const quizPostRequest = require("./quiz/quiz-postrequest.js"),
+  fetchData = require("./helper/helper-fetch.js"),
+  quizCalc = require("./quiz/quiz-calculation.js")
+
 let allSports
 ;(async function() {
   const getAllSports = require("./sportlist/sportlist-az-list.js")
   allSports = await getAllSports()
 })()
-
-const quizPostRequest = require("./quiz/quiz-postrequest.js"),
-  fetchData = require("./helper/helper-fetch.js")
 
 module.exports = (app, eventsData) => {
   app.get("/", (req, res) => {
@@ -53,6 +54,21 @@ module.exports = (app, eventsData) => {
   })
   app.get("/quiz", (req, res) => {
     res.render("pages/quiz/quiz-questions.ejs", {
+      hero: "small-hero",
+      heroText: ["Amsterdam", "Zuid-Oost", "Be a part of it!"]
+    })
+  })
+  app.post("/quiz", async function(req, res) {
+    const sportQuizData = await fetchData(
+      "https://raw.githubusercontent.com/RobinStut/meesterproef/development/data/json/sportQuizFilter.json"
+    )
+
+    const quizResult = quizCalc(req, sportQuizData)
+    // console.log(test)
+
+    res.render("pages/quiz/quiz-result.ejs", {
+      quizResult: quizResult,
+      sportQuizData: sportQuizData,
       hero: "small-hero",
       heroText: ["Amsterdam", "Zuid-Oost", "Be a part of it!"]
     })
