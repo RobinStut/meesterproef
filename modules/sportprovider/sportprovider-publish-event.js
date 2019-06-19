566a8ef6e516b42242aee38da7fc636d73221a28
RobinStut <robin.stut@hva.nl>
2019-06-19 11:11:10 +0200

    refactor/removing commits

--- modules/sportprovider/sportprovider-publish-event.js
+++ modules/sportprovider/sportprovider-publish-event.js
@@ -37,44 +37,61 @@ module.exports = (app, conceptEvents, eventsData) => {
 }
 
 module.exports = (app, fs, conceptEvents, eventsData) => {
-    app.post('/publish-event', (req, res) => {
-        if (req.url === '/publish-event' && eventsData.length === 0) {
-            eventsData.push(conceptEvents[0])   
+  app.post("/publish-event", (req, res) => {
+    if (req.url === "/publish-event" && eventsData.length === 0) {
+      eventsData.push(conceptEvents[0])
 
-            console.log(eventsData)
-
-            fs.writeFile('./data/json/sportEvents.json', JSON.stringify(eventsData), err => {
-                if(err) {
-                    return console.log(err) 
-                }
-            })
-
-            conceptEvents.length = 0
-
-            console.log('Event Data:')
-            console.log(eventsData)
+      fs.writeFile(
+        "./data/json/sportEvents.json",
+        JSON.stringify(eventsData),
+        err => {
+          if (err) {
+            return console.log(err)
+          }
+        }
+      )
 
             res.redirect('/events')
         } else if (eventsData.length > 0) {
             console.log(conceptEvents)
+      conceptEvents.length = 0
 
             const exists = eventsData.find(e => {
                 return e.general.title.toLowerCase() === conceptEvents[0].general.title.toLowerCase()
             })
             if (!exists) {
                 eventsData.push(conceptEvents[0])
+      res.redirect("/events")
+    } else if (eventsData.length > 0) {
+      const exists = eventsData.find(e => {
+        return (
+          e.general.title.toLowerCase() ===
+          conceptEvents[0].general.title.toLowerCase()
+        )
+      })
+      if (!exists) {
+        eventsData.push(conceptEvents[0])
 
                 conceptEvents.length = 0
+        conceptEvents.length = 0
 
                 console.log('Event Data:')
                 console.log(eventsData)
+        console.log("Event Data:")
 
                 res.redirect('/')
             } else {
                 console.log("Sorry, that event already exists.")
+        res.redirect("/")
+      } else {
+        console.log("Sorry, that event already exists.")
 
                 res.redirect('/')
             }
         }
     })
+        res.redirect("/")
+      }
+    }
+  })
 }
