a82b1bf65f7468f14f702731c8bdd41c1e7be9cb
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-20 15:16:01 +0200

    prettier(script): Prettier stuff

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,11 @@
 import header from "./modules/header/header.js"
 import Quiz from "./modules/quiz/quiz-setup.js"
-import QuizSplit from "./modules/quiz/quiz-splitup.js"
 import CustomSlider from "./modules/custom-slider.js"
 
 const customSliders = document.getElementsByClassName("custom-slider")
 
 for (let i = 0; i < customSliders.length; i++) {
-  new CustomSlider(customSliders[i], (origin, value) => {
-    origin.slider.pin.textContent = value
-  })
+	new CustomSlider(customSliders[i], (origin, value) => {
+		origin.slider.pin.textContent = value
+	})
 }
