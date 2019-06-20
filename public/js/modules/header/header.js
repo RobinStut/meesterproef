135296d959aad3f2237b661fa54407c1a6879fe5
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-20 15:13:14 +0200

    refactor(header): Removed search icon class toggle

--- public/js/modules/header/header.js
+++ public/js/modules/header/header.js
@@ -1,35 +1,32 @@
 function sidebarNav() {
-  const body = document.querySelector("body"),
-      headerHamburger = document.getElementById("st-hamburger-menu"),
-      headerHamburgerIcon = document.querySelector("#st-hamburger-menu svg"),
-      asideMenu = document.querySelector("aside")
+	const body = document.querySelector("body"),
+		headerHamburger = document.getElementById("st-hamburger-menu"),
+		headerHamburgerIcon = document.querySelector("#st-hamburger-menu svg"),
+		asideMenu = document.querySelector("aside")
 
-  headerHamburger.addEventListener("click", () => {
-      body.classList.toggle("st-overflow-hidden")
-      asideMenu.classList.toggle("st-show")
-      headerHamburgerIcon.classList.toggle("st-text-color")
-  })
+	headerHamburger.addEventListener("click", () => {
+		body.classList.toggle("st-overflow-hidden")
+		asideMenu.classList.toggle("st-show")
+		headerHamburgerIcon.classList.toggle("st-text-color")
+	})
 }
 
 function headerBackgroundScroll() {
-    const scrollPos = 0,
-          header = document.querySelector("header"),
-          headerHamburgerIcon = document.querySelector("#st-hamburger-menu svg"),
-          headerSearchIcon = document.querySelector("#st-search svg")
+	const scrollPos = 0,
+		header = document.querySelector("header"),
+		headerHamburgerIcon = document.querySelector("#st-hamburger-menu svg")
 
-    window.addEventListener("scroll", () => {
-        const windowYOffset = window.scrollY
+	window.addEventListener("scroll", () => {
+		const windowYOffset = window.scrollY
 
-        if (windowYOffset > scrollPos){
-            header.classList.add("st-header-background-color")
-            headerHamburgerIcon.classList.add("st-text-color")
-            headerSearchIcon.classList.add("st-text-color")
-        } else if (windowYOffset === scrollPos) {
-            header.classList.remove("st-header-background-color")
-            headerHamburgerIcon.classList.remove("st-text-color")
-            headerSearchIcon.classList.remove("st-text-color")
-        }
-    })
+		if (windowYOffset > scrollPos) {
+			header.classList.add("st-header-background-color")
+			headerHamburgerIcon.classList.add("st-text-color")
+		} else if (windowYOffset === scrollPos) {
+			header.classList.remove("st-header-background-color")
+			headerHamburgerIcon.classList.remove("st-text-color")
+		}
+	})
 }
 
 headerBackgroundScroll()
