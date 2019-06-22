4eb42bcfa70474179f2b66e646674657e13f664b
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-22 19:27:38 +0200

    feature(interactive-cards): Finished the logic

--- public/js/modules/interactive-card.js
+++ public/js/modules/interactive-card.js
@@ -1,35 +1,50 @@
 import DraggingEvent from "./dragging-event.js"
 
 export default class extends DraggingEvent {
-  constructor(card) {
+  constructor(card, onEmpty = null) {
     super(card)
 
     this.card = card
+    this.container = card.parentElement
+    this.onEmpty = onEmpty
 
     this.memory
 
     super.getDistance(this.slideCard.bind(this))
   }
 
+  checkAmount() {
+    if (this.container.children.length === 0) {
+      this.onEmpty()
+    }
+  }
+
   removeCard(toRight) {
+    this.container.style.overflow = "hidden"
+
     this.card.style.height = `${this.card.offsetHeight}px`
 
+    if (toRight) {
+      this.card.style.transform = `translateX(${window.innerWidth}px)`
+    } else {
+      this.card.style.transform = `translateX(-${window.innerWidth}px)`
+    }
+
     setTimeout(() => {
-      this.card.style.height = "0px"
       this.card.style.margin = "0px"
       this.card.style.padding = "0px"
       this.card.style.border = "none"
-
-      if (toRight) {
-        this.card.style.transform = `translateX(${window.innerWidth}px)`
-      } else {
-        this.card.style.transform = `translateX(-${window.innerWidth}px)`
-      }
-    }, 0)
+      this.card.style.height = "0px"
+    }, 250)
 
     setTimeout(() => {
+      this.container.style.overflow = "initial"
       this.card.remove()
-    }, 1000)
+
+      if (this.onEmpty) {
+        this.checkAmount()
+      }
+    }, 500)
   }
 
   slideCard(data) {
