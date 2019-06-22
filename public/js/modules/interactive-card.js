a5687e7981a6c999a2295e9b095f3a6e6ccad5a2
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-06-22 18:14:31 +0200

    feature(interactive-cards): Can now move the cards left or right

--- public/js/modules/interactive-card.js
+++ public/js/modules/interactive-card.js
@@ -1,10 +1,30 @@
 import DraggingEvent from "./dragging-event.js"
 
 export default class extends DraggingEvent {
-  constructor(container) {
-    super()
+  constructor(card) {
+    super(card)
 
-    this.container = container
-    this.cards = container.querySelectorAll(".card")
+    this.card = card
+
+    this.memory
+
+    super.getDistance(this.slideCard.bind(this))
+  }
+
+  updateCard
+
+  slideCard(data) {
+    if (data !== null) {
+      this.memory = data
+      this.card.classList.remove("smooth-return")
+      this.card.style.transform = `translateX(${data.x}px)`
+    } else {
+      if (Math.abs(this.memory.x) > 500) {
+        console.log("past max")
+      } else {
+        this.card.classList.add("smooth-return")
+        this.card.style.transform = `translateX(0)`
+      }
+    }
   }
 }
--- public/js/stacked-cards.js
+++
@@ -1,5 +0,0 @@
-import DraggingEvent from "./dragging-event.js"
-
-export class extends DraggingEvent {
-  super()
-}
