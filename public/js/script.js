14805ffe2567ccb7865f59bf6bd92b797878b3b9
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-23 19:26:49 +0200

    feature(class-toggle): Added click toggle helper function

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+import { classToggleClick } from "./modules/helpers/class-toggle-click.js"
 import header from "./modules/header/header.js"
 import Quiz from "./modules/quiz/quiz-setup.js"
 import CustomSlider from "./modules/custom-slider.js"
@@ -9,3 +10,12 @@ for (let i = 0; i < customSliders.length; i++) {
 		origin.slider.pin.textContent = value
 	})
 }
+
+const eventsFilter = document.querySelector(".st-filter-form")
+const eventsFilterButton = document.querySelector(".st-filter-form button")
+const toggleElements = [
+	{ element: eventsFilter, class: "st-filter-form-show" },
+	{ element: eventsFilterButton, class: "st-turn-180" }
+]
+
+classToggleClick(eventsFilterButton, toggleElements)
