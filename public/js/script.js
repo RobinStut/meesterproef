9f167519fe18fcd0db0f5e28e5a0a746c9f02e13
RobinStut <robin.stut@hva.nl>
2019-06-24 12:21:52 +0200

    feature/adding slider texts

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ import header from "./modules/header/header.js"
 import Quiz from "./modules/quiz/quiz-setup.js"
 import QuizSliderTexts from "./modules/quiz/quiz-slider-texts.js"
 import CustomSlider from "./modules/custom-slider.js"
-import InteractiveCards from "./modules/interactive-card.js"
+import InteractiveCards from "./modules/interactive-cards.js"
 import quizSliderTexts from "./modules/quiz/quiz-slider-texts.js"
 
 const customSliders = document.getElementsByClassName("custom-slider")
@@ -11,9 +11,7 @@ const cardsContainer = document.querySelector(".interactive-cards")
 for (let i = 0; i < customSliders.length; i++) {
   new CustomSlider(customSliders[i], (origin, value) => {
     origin.slider.pin.textContent = value
-    // console.log(origin.slider.pin)
-
-    quizSliderTexts(value)
+    quizSliderTexts(value, origin.id)
   })
 }
 
