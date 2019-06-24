f4e77cd323debb9fd0e0cc7d629564755a6dc1cc
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-06-24 12:08:18 +0200

    refactor: cleaned up console.logjes

--- modules/helper/helper-fetch.js
+++ modules/helper/helper-fetch.js
@@ -3,10 +3,8 @@ const fetch = require("node-fetch")
 module.exports = (urlToFetch, dataObject) => {
   return new Promise(async (resolve, reject) => {
     if (dataObject.length !== 0) {
-      console.log("Er is al data")
       resolve(dataObject)
     } else {
-      console.log("Er is nog geen data, dus data ophalen")
       const response = await fetch(urlToFetch)
       let data = await response.json()
       resolve(data)
