7e9b6d0ec3a8bf2773792efe359f8688e5a5426b
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-25 15:49:17 +0200

    feature(nav): added amsterdam crosses to <header

--- public/js/modules/header/header.js
+++ public/js/modules/header/header.js
@@ -1,40 +1,42 @@
 function sidebarNav() {
-  const body = document.querySelector("body"),
-    headerHamburger = document.getElementsByClassName("header__hamburger"),
-    headerHamburgerIcon = document.querySelector(".header__hamburger svg"),
-    asideMenu = document.getElementsByClassName("sidebar"),
-    headerLogo = document.getElementsByClassName("header__logo"),
-    header = document.querySelector("header")
+	const body = document.querySelector("body"),
+		headerHamburger = document.getElementsByClassName("header__hamburger"),
+		headerHamburgerIcon = document.querySelector(".header__hamburger svg"),
+		asideMenu = document.getElementsByClassName("sidebar"),
+		headerLogo = document.getElementsByClassName("header__logo"),
+		header = document.querySelector("header")
 
-  headerHamburger[0].addEventListener("click", () => {
-    headerLogo[0].classList.toggle("display--none")
-    body.classList.toggle("body--overflow-hidden")
-    asideMenu[0].classList.toggle("sidebar--show")
-    header.classList.toggle("transparent")
+	headerHamburger[0].addEventListener("click", () => {
+		headerLogo[0].classList.toggle("display--none")
+		body.classList.toggle("body--overflow-hidden")
+		asideMenu[0].classList.toggle("sidebar--show")
+		header.classList.toggle("transparent")
 
-    headerHamburgerIcon.classList.add("header__hamburger--blue")
-  })
+		headerHamburgerIcon.classList.add("header__hamburger--blue")
+	})
 }
 
 function headerBackgroundScroll() {
-  const scrollPos = 0,
-    header = document.getElementsByClassName("header"),
-    headerHamburgerIcon = document.querySelector(".header__hamburger svg"),
-    headerLogo = document.getElementsByClassName("header__logo")
+	const scrollPos = 0,
+		header = document.getElementsByClassName("header"),
+		headerHamburgerIcon = document.querySelector(".header__hamburger svg"),
+		headerLogo = document.getElementsByClassName(
+			"a7a2a367-2ac2-4252-9aa9-a4bc92e07ffb"
+		)
 
-  window.addEventListener("scroll", () => {
-    const windowYOffset = window.scrollY
+	window.addEventListener("scroll", () => {
+		const windowYOffset = window.scrollY
 
-    if (windowYOffset > scrollPos) {
-      header[0].classList.add("header--background-solid")
-      headerHamburgerIcon.classList.add("header__hamburger--blue")
-      headerLogo[0].classList.add("header__hamburger--blue")
-    } else if (windowYOffset === scrollPos) {
-      header[0].classList.remove("header--background-solid")
-      headerHamburgerIcon.classList.remove("header__hamburger--blue")
-      headerLogo[0].classList.remove("header__hamburger--blue")
-    }
-  })
+		if (windowYOffset > scrollPos) {
+			header[0].classList.add("header--background-solid")
+			headerHamburgerIcon.classList.add("header__hamburger--blue")
+			headerLogo[0].classList.add("header__logo__svg--blue")
+		} else if (windowYOffset === scrollPos) {
+			header[0].classList.remove("header--background-solid")
+			headerHamburgerIcon.classList.remove("header__hamburger--blue")
+			headerLogo[0].classList.remove("header__logo__svg--blue")
+		}
+	})
 }
 
 headerBackgroundScroll()
